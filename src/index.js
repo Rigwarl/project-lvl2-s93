6200173cdad8ec926b7de48b480bfa2618391a92
Rigwarl <Avips@mail.ru>
2017-05-02 17:32:57 +0300

    add flat gendiff

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-export default () => 'placeholder';
+export default (before, after) => {
+  const oldLines = Object.keys(before)
+    .filter(key => before[key] === after[key])
+    .map(key => `   ${key}: ${before[key]}`);
+
+  const changedLines = Object.keys(before)
+    .filter(key => (before[key] !== after[key]) && (after[key] !== undefined))
+    .map(key => ` - ${key}: ${before[key]}\n + ${key}: ${after[key]}`);
+
+  const removedLines = Object.keys(before)
+    .filter(key => after[key] === undefined)
+    .map(key => ` - ${key}: ${before[key]}`);
+
+  const newLines = Object.keys(after)
+    .filter(key => before[key] === undefined)
+    .map(key => ` + ${key}: ${after[key]}`);
+
+  const result = [...oldLines, ...changedLines, ...removedLines, ...newLines].join('\n');
+
+  return `{\n${result}\n}`;
+};
