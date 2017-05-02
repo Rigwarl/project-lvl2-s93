86647bef101dca54ed7f6b0632a7ed308c7ac8f4
Rigwarl <Avips@mail.ru>
2017-05-02 15:32:40 +0300

    get version from package.json

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import config from '../../package.json';
 
 program
-  .version('0.2.0')
+  .version(config.version)
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
