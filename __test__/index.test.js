ca84cb53494bfa64e2020622888856bc61b49845
Rigwarl <Avips@mail.ru>
2017-05-02 16:37:35 +0300

    add more tests

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,20 +1,39 @@
 import gendiff from '../src/index';
 
+const before = {
+  host: 'hexlet.io',
+  timeout: 50,
+  proxy: '123.234.53.22',
+};
+const after = {
+  timeout: 20,
+  verbose: true,
+  host: 'hexlet.io',
+};
+
 test('empty configs', () => {
   expect(gendiff({}, {})).toBe('');
 });
 
+test('first empty config', () => {
+  const result = `{
+    + timeout: 20,
+    + verbose: true,
+    + host: 'hexlet.io',
+  }`;
+  expect(gendiff({}, after)).toBe(result);
+});
+
+test('second empty config', () => {
+  const result = `{
+    - host: 'hexlet.io',
+    - timeout: 50,
+    - proxy: '123.234.53.22',
+  }`;
+  expect(gendiff(before, {})).toBe(result);
+});
+
 test('custom configs', () => {
-  const before = {
-    host: 'hexlet.io',
-    timeout: 50,
-    proxy: '123.234.53.22',
-  };
-  const after = {
-    timeout: 20,
-    verbose: true,
-    host: 'hexlet.io',
-  };
   const result = `{
       host: hexlet.io
     + timeout: 20
@@ -22,6 +41,5 @@ test('custom configs', () => {
     - proxy: 123.234.53.22
     + verbose: true
   }`;
-
   expect(gendiff(before, after)).toBe(result);
 });
