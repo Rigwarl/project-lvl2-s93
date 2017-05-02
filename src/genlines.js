b2a9f0b73b7080725271d9deb99251d07a95f822
Rigwarl <Avips@mail.ru>
2017-05-03 01:20:37 +0300

    improve genlines algorithm

--- src/genlines.js
+++ src/genlines.js
@@ -1,19 +1,25 @@
 export default (before, after) => {
-  const oldLines = Object.keys(before)
-    .filter(key => before[key] === after[key])
-    .map(key => `   ${key}: ${before[key]}`);
+  const lineDiff = {
+    same: key => [`   ${key}: ${before[key]}`],
+    new: key => [` + ${key}: ${after[key]}`],
+    removed: key => [` - ${key}: ${before[key]}`],
+    changed: key => [` - ${key}: ${before[key]}`, ` + ${key}: ${after[key]}`],
+  };
 
-  const changedLines = Object.keys(before)
-    .filter(key => (before[key] !== after[key]) && (after[key] !== undefined))
-    .map(key => ` - ${key}: ${before[key]}\n + ${key}: ${after[key]}`);
+  const keyCheck = {
+    same: key => before[key] === after[key],
+    new: key => before[key] === undefined,
+    removed: key => after[key] === undefined,
+    changed: () => true,
+  };
 
-  const removedLines = Object.keys(before)
-    .filter(key => after[key] === undefined)
-    .map(key => ` - ${key}: ${before[key]}`);
+  const getKeyType = key => ['same', 'new', 'removed', 'changed']
+    .find(type => keyCheck[type](key));
 
-  const newLines = Object.keys(after)
-    .filter(key => before[key] === undefined)
-    .map(key => ` + ${key}: ${after[key]}`);
+  const result = Object.keys({ ...before, ...after })
+    .map(key => [key, getKeyType(key)])
+    .map(([key, type]) => lineDiff[type](key))
+    .reduce((acc, curr) => [...acc, ...curr], []);
 
-  return [...oldLines, ...changedLines, ...removedLines, ...newLines];
+  return result;
 };
