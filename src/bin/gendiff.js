b8e8d849a3c6bdd74a39b6a1c42cdd6d658086e5
Rigwarl <Avips@mail.ru>
2017-05-02 16:32:46 +0300

    read & parse config files

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
 import program from 'commander';
 import config from '../../package.json';
 import gendiff from '../index';
+
+const getConfig = path => JSON.parse(fs.readFileSync(path));
 
 program
   .version(config.version)
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
-  .action((first, second) => console.log(gendiff(first, second)))
+  .action((first, second) => console.log(gendiff(getConfig(first), getConfig(second))))
   .parse(process.argv);
