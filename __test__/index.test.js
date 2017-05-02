18df65861bb6bbd98ee1f33009dd458ca2c66426
Rigwarl <Avips@mail.ru>
2017-05-02 17:31:46 +0300

    fix tests

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -11,35 +11,37 @@ const after = {
   host: 'hexlet.io',
 };
 
+const getResultStr = arr => `{\n${arr.join('\n')}\n}`;
+
 test('empty configs', () => {
-  expect(gendiff({}, {})).toBe('');
+  expect(gendiff({}, {})).toBe('{\n\n}');
 });
 
 test('first empty config', () => {
-  const result = `{
-    + timeout: 20,
-    + verbose: true,
-    + host: 'hexlet.io',
-  }`;
-  expect(gendiff({}, after)).toBe(result);
+  const result = [
+    ' + timeout: 20',
+    ' + verbose: true',
+    ' + host: hexlet.io',
+  ];
+  expect(gendiff({}, after)).toBe(getResultStr(result));
 });
 
 test('second empty config', () => {
-  const result = `{
-    - host: 'hexlet.io',
-    - timeout: 50,
-    - proxy: '123.234.53.22',
-  }`;
-  expect(gendiff(before, {})).toBe(result);
+  const result = [
+    ' - host: hexlet.io',
+    ' - timeout: 50',
+    ' - proxy: 123.234.53.22',
+  ];
+  expect(gendiff(before, {})).toBe(getResultStr(result));
 });
 
 test('custom configs', () => {
-  const result = `{
-      host: hexlet.io
-    + timeout: 20
-    - timeout: 50
-    - proxy: 123.234.53.22
-    + verbose: true
-  }`;
-  expect(gendiff(before, after)).toBe(result);
+  const result = [
+    '   host: hexlet.io',
+    ' - timeout: 50',
+    ' + timeout: 20',
+    ' - proxy: 123.234.53.22',
+    ' + verbose: true',
+  ];
+  expect(gendiff(before, after)).toBe(getResultStr(result));
 });
