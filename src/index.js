9accbd531d4d2f6f85c66e3b78e349ff22a04b0e
Rigwarl <Avips@mail.ru>
2017-05-03 18:13:30 +0300

    add ini format support

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
+import ini from 'ini';
 import genLines from './genlines';
 
 const parse = {
+  ini: file => ini.parse(file),
   yml: file => yaml.safeLoad(file) || {},
   json: file => JSON.parse(file),
 };
 const getExt = path => path.split('.').slice(-1)[0];
-const getConfig = path => parse[getExt(path)](fs.readFileSync(path));
+const getConfig = path => parse[getExt(path)](fs.readFileSync(path, 'utf-8'));
 
 export default (before, after) => {
   const lines = genLines(getConfig(before), getConfig(after));
