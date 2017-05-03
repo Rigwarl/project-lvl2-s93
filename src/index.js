cbe1bf0c8e5598ec0e367332b3cd94964e612572
Rigwarl <Avips@mail.ru>
2017-05-03 16:34:02 +0300

    up vers

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import fs from 'fs';
 import yaml from 'js-yaml';
 import genLines from './genlines';
 
-const getExt = path => path.split('.').slice(-1)[0];
 const parse = {
   yml: file => yaml.safeLoad(file) || {},
   json: file => JSON.parse(file),
 };
+const getExt = path => path.split('.').slice(-1)[0];
 const getConfig = path => parse[getExt(path)](fs.readFileSync(path));
 
 export default (before, after) => {
