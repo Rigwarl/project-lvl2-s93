01d6fb39e776663ff9ad9cd4f2fb661743c9ff94
Rigwarl <Avips@mail.ru>
2017-05-04 17:17:00 +0300

    add AST

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,27 +1,51 @@
 import _ from 'lodash';
 
-export default (before, after) => {
-  const keyDiff = {
-    same: key => [`   ${key}: ${before[key]}`],
-    new: key => [` + ${key}: ${after[key]}`],
-    removed: key => [` - ${key}: ${before[key]}`],
-    changed: key => [` - ${key}: ${before[key]}`, ` + ${key}: ${after[key]}`],
-  };
+const keyChecker = {
+  same: (before, after) =>
+    before === after || (before instanceof Object && after instanceof Object),
+  new: before => before === undefined,
+  removed: (before, after) => after === undefined,
+  changed: () => true,
+};
 
-  const keyCheck = {
-    same: key => before[key] === after[key],
-    new: key => before[key] === undefined,
-    removed: key => after[key] === undefined,
-    changed: () => true,
-  };
+const getKeyType = (before, after) => ['same', 'new', 'removed', 'changed']
+  .find(type => keyChecker[type](before, after));
 
-  const getKeyType = key => ['same', 'new', 'removed', 'changed']
-    .find(type => keyCheck[type](key));
+const buildKeyDiff = (name, before, after, depth) => ({
+  name,
+  depth,
+  type: getKeyType(before, after),
+  before: before instanceof Object ? buildDiff(before, after, depth + 1) : before,
+  after: after instanceof Object ? buildDiff(before, after, depth + 1) : after,
+});
 
-  const getKeyDiff = key => keyDiff[getKeyType(key)](key);
+export const buildDiff = (before, after, depth = 0) =>
+  _.union(Object.keys(before), Object.keys(after))
+  .map(key => buildKeyDiff(key, before[key], after[key], depth));
 
-  const keys = _.union(Object.keys(before), Object.keys(after));
-  const keysDiff = keys.map(getKeyDiff);
+const keyBuilder = {
+  same: key => [`   ${key.name}: ${stringifyDiff(key.before)}`],
+  new: key => [` + ${key.name}: ${stringifyDiff(key.after)}`],
+  removed: key => [` - ${key.name}: ${stringifyDiff(key.before)}`],
+  changed: key => [
+    ` - ${key.name}: ${stringifyDiff(key.before)}`,
+    ` + ${key.name}: ${stringifyDiff(key.after)}`
+  ],
+};
 
-  return _.flatten(keysDiff);
+const stringifyKey = (key) => {
+  const pad = _.repeat(' ', key.depth * 3);
+  const diff = _.flatten(keyBuilder[key.type](key));
+
+  return diff.map(row => pad + row).join('\n');
+};
+
+export const stringifyDiff = (diff) => {
+  if (typeof diff === 'string' || typeof diff === 'number' || typeof diff === 'boolean') {
+    return diff;
+  }
+
+  const rows = diff.map(key => stringifyKey(key));
+
+  return `{\n${rows.join('\n')}\n}`;
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 
-import gendiff from './gendiff';
+import { buildDiff, stringifyDiff } from './gendiff';
 import getParser from './parsers';
 
 const getExt = p => path.extname(p).substr(1);
 const getConfig = p => getParser(getExt(p))(fs.readFileSync(p, 'utf-8'));
 
 export default (before, after) => {
-  const lines = gendiff(getConfig(before), getConfig(after));
+  const diff = buildDiff(getConfig(before), getConfig(after));
 
-  return `{\n${lines.join('\n')}\n}`;
+  return stringifyDiff(diff);
 };
