4c2422921534eb1472ab0473b6a3ff9492e92db6
Rigwarl <Avips@mail.ru>
2017-05-05 15:21:50 +0300

    add plain format builder

--- src/diff-builders/plain.js
+++ src/diff-builders/plain.js
@@ -1,3 +1,28 @@
-const build = (before, after) => '{\n\n}';
+const build = (node, path) => {
+  if (!(node instanceof Object)) {
+    return null;
+  }
+  // eslint-disable-next-line no-use-before-define
+  const rows = node.map(key => buildKey(key, path)).filter(key => !!key);
 
-export default build;
+  return rows.join('\n');
+};
+
+const buildValue = (value, add = false) => {
+  if (value instanceof Object) {
+    return 'complex value';
+  }
+
+  return add ? `value: ${value}` : value;
+};
+
+const keyBuilder = {
+  same: (key, path) => build(key.before, path),
+  new: (key, path) => `Property '${path.join('.')}' was added with ${buildValue(key.after, true)}`,
+  removed: (key, path) => `Property '${path.join('.')}' was removed`,
+  changed: (key, path) => `Property '${path.join('.')}' was updated. From '${buildValue(key.before)}' to '${buildValue(key.after)}'`,
+};
+
+const buildKey = (key, path) => keyBuilder[key.type](key, path.concat(key.name));
+
+export default ast => build(ast, []);
