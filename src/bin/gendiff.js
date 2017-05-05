5ac1ef5c1a274263c4888340621593ca85ee8402
Rigwarl <Avips@mail.ru>
2017-05-05 12:57:15 +0300

    pass format arg

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,7 +7,7 @@ import gendiff from '..';
 program
   .version(config.version)
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'Output format')
+  .option('-f, --format [type]', 'Output format, padded (default) or plain')
   .arguments('<firstConfig> <secondConfig>')
-  .action((first, second) => console.log(gendiff(first, second)))
+  .action((first, second) => console.log(gendiff(first, second, program.format)))
   .parse(process.argv);
