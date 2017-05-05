1323123ff31ce9cda976956af7c57feff99a95d3
Rigwarl <Avips@mail.ru>
2017-05-05 18:50:05 +0300

    add json format

--- src/diff-builders/index.js
+++ src/diff-builders/index.js
@@ -1,9 +1,11 @@
 import padded from './padded';
 import plain from './plain';
+import json from './json';
 
 const builders = {
   padded,
   plain,
+  json,
 };
 
 export default format => builders[format];
---
+++ src/diff-builders/json.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+
+const build = (node) => {
+  if (typeof node === 'string' || typeof node === 'number' || typeof node === 'boolean') {
+    return node;
+  }
+
+  // eslint-disable-next-line no-use-before-define
+  const diff = node.map(key => buildKey(key));
+  const flatDiff = _.flatten(diff);
+
+  return flatDiff.reduce((acc, curr) => ({ ...acc, ...curr }), {});
+};
+
+const keyBuilder = {
+  same: key => [{ [key.name]: build(key.before) }],
+  new: key => [{ [`+${key.name}`]: build(key.after) }],
+  removed: key => [{ [`-${key.name}`]: build(key.before) }],
+  changed: key => [
+    { [`-${key.name}`]: build(key.before) },
+    { [`+${key.name}`]: build(key.after) },
+  ],
+};
+
+const buildKey = key => keyBuilder[key.type](key);
+
+export default ast => JSON.stringify(build(ast), null, '  ');
